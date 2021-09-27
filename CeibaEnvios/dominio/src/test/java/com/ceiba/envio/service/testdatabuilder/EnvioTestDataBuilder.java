6e5feef798fe2359c5630509609202827aa5cb6a
JohanSandoval <johan.sandoval@ceiba.com.co>
2021-09-27 13:30:22 -0500

    test dominio envio

--- CeibaEnvios/dominio/src/test/java/com/ceiba/envio/service/testdatabuilder/EnvioTestDataBuilder.java
+++ CeibaEnvios/dominio/src/test/java/com/ceiba/envio/service/testdatabuilder/EnvioTestDataBuilder.java
@@ -2,6 +2,7 @@ package com.ceiba.envio.service.testdatabuilder;
 
 import com.ceiba.destinatario.servicio.testdatabuilder.DestinatarioTestDataBuilder;
 import com.ceiba.destinatatio.modelo.entidad.Destinatario;
+import com.ceiba.envio.modelo.entidad.Envio;
 import com.ceiba.remitente.modelo.entidad.Remitente;
 import com.ceiba.remitente.servicio.testdatabuilder.RemitenteTestDataBuilder;
 
@@ -12,10 +13,10 @@ public class EnvioTestDataBuilder {
     private static final Destinatario DESTINATARIO = new DestinatarioTestDataBuilder().build();
     private static final Double PESO = 10D;
 
+    private Long id;
     private Remitente remitente;
     private Destinatario destinatario;
     private double peso;
-
 
     public EnvioTestDataBuilder() {
 
@@ -24,5 +25,23 @@ public class EnvioTestDataBuilder {
         this.peso = PESO;
     }
 
+    public EnvioTestDataBuilder conPeso(Double peso){
+        this.peso = peso;
+        return this;
+    }
+
+    public EnvioTestDataBuilder conRemitente(Remitente remitente){
+        this.remitente = remitente;
+        return this;
+    }
+
+    public EnvioTestDataBuilder conDestinatario(Destinatario destinatario){
+        this.destinatario = destinatario;
+        return this;
+    }
+
+    public Envio build(){
+        return new Envio(id, remitente, destinatario, peso);
+    }
 
 }
