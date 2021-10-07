6338516320faf33be42435e5739a0766b0e4ee00
JohanSandoval <johan.sandoval@ceiba.com.co>
2021-10-07 16:14:30 -0500

    ajuste orden test

--- CeibaEnvios/infraestructura/src/test/java/com/ceiba/ciudad/controlador/ComandoControladorCiudadTest.java
+++ CeibaEnvios/infraestructura/src/test/java/com/ceiba/ciudad/controlador/ComandoControladorCiudadTest.java
@@ -30,7 +30,7 @@ public class ComandoControladorCiudadTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
     @Test
     public void crear() throws Exception{
         //arrange
@@ -43,7 +43,7 @@ public class ComandoControladorCiudadTest {
                 .andExpect(content().json("{'valor': 3}"));
     }
 
-    @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
     @Test
     public void actualizar() throws Exception{
         //arrange
